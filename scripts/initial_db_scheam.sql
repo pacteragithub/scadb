648af2a9f9b2c1266bb2ded32d67f689e911c7e4
Louis Zhang <cool.yzhang@gmail.com>
2018-11-16 10:56:23 -0800

    modified the rule table column

--- scripts/initial_db_scheam.sql
+++ scripts/initial_db_scheam.sql
@@ -3,6 +3,15 @@ CREATE DATABASE sca;
 USE sca;
 
 DROP TABLE IF EXISTS `business_model`;
+DROP TABLE IF EXISTS `score_item`;
+DROP TABLE IF EXISTS `sub_score_item`;
+DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `regular_expression`;
+DROP TABLE IF EXISTS `keywords`;
+DROP TABLE IF EXISTS `speed_detection`;
+DROP TABLE IF EXISTS `interrupt`;
+DROP TABLE IF EXISTS `rule`;
+
 CREATE TABLE `business_model` (
   `business_model_id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `label`             VARCHAR(20)                           NOT NULL
@@ -11,7 +20,7 @@ CREATE TABLE `business_model` (
 INSERT INTO `business_model` VALUES (1, 'General');
 INSERT INTO `business_model` VALUES (2, 'Renew');
 
-DROP TABLE IF EXISTS `category`;
+
 CREATE TABLE `category` (
   `category_id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `label`       VARCHAR(20)                           NOT NULL
@@ -21,13 +30,13 @@ INSERT INTO `category` VALUES (1, 'All');
 INSERT INTO `category` VALUES (2, 'Pending');
 INSERT INTO `category` VALUES (3, 'Exception');
 
-DROP TABLE IF EXISTS `score_item`;
+
 CREATE TABLE `score_item` (
   `score_item_id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `label`         VARCHAR(20)                           NOT NULL
 );
 
-DROP TABLE IF EXISTS `sub_score_item`;
+
 CREATE TABLE `sub_score_item` (
   `sub_score_item_id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `score_item_id`     BIGINT(20)                            NOT NULL REFERENCES score_item (score_item_id),
@@ -35,7 +44,7 @@ CREATE TABLE `sub_score_item` (
   `score`             SMALLINT(6)                           NOT NULL
 );
 
-DROP TABLE IF EXISTS `rule`;
+
 CREATE TABLE `rule` (
   `rule_id`           BIGINT(20)   NOT NULL PRIMARY KEY  AUTO_INCREMENT,
   `rule_name`         VARCHAR(255) NOT NULL,
@@ -43,11 +52,10 @@ CREATE TABLE `rule` (
   `category_id`       BIGINT(20)                         DEFAULT NULL REFERENCES category (category_id),
   `effective_time`    VARCHAR(20)                        DEFAULT NULL,
   `assessment_points` VARCHAR(225) NOT NULL,
-  `sub_score_item_id` BIGINT(20)                         DEFAULT NULL REFERENCES sub_score_item (sub_score_item_id),
-  `rule_type`         VARCHAR(10)  NOT NULL
+  `sub_score_item_id` BIGINT(20)                         DEFAULT NULL REFERENCES sub_score_item (sub_score_item_id)
 );
 
-DROP TABLE IF EXISTS `keywords`;
+
 CREATE TABLE `keywords` (
   `keywords_id` BIGINT(20)   NOT NULL,
   `occurence`   VARCHAR(20)  NOT NULL,
@@ -55,21 +63,21 @@ CREATE TABLE `keywords` (
   FOREIGN KEY (`keywords_id`) REFERENCES rule (rule_id)
 );
 
-DROP TABLE IF EXISTS `regular_expression`;
+
 CREATE TABLE `regular_expression` (
   `regular_expression_id` BIGINT(20)   NOT NULL,
   `regular_expression`    VARCHAR(255) NOT NULL,
   FOREIGN KEY (`regular_expression_id`) REFERENCES rule (rule_id)
 );
 
-DROP TABLE IF EXISTS `speed_detection`;
+
 CREATE TABLE `speed_detection` (
   `speed_detection_id`  BIGINT(20) NOT NULL,
   `max_word_per_minute` SMALLINT(6) DEFAULT NULL,
   FOREIGN KEY (`speed_detection_id`) REFERENCES rule (rule_id)
 );
 
-DROP TABLE IF EXISTS `interrupt`;
+
 CREATE TABLE `interrupt` (
   `interrupt_id`     BIGINT(20) NOT NULL,
   `overlap_time_sec` SMALLINT(6) DEFAULT NULL,
